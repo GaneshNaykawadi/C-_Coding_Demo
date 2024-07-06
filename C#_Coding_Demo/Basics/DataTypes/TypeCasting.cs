e272bc47106a4537c7fd611fa06c47595ef19a23
ganeshnaykawadi <ganeshnaykawadi@gmail.com>
2024-07-06 23:10:51 +0530

    TypeCasting Exercise Day1 Completed.. some changes needed.

--- C#_Coding_Demo/Basics/DataTypes/TypeCasting.cs
+++ C#_Coding_Demo/Basics/DataTypes/TypeCasting.cs
@@ -1,6 +1,51 @@
 namespace C__Coding_Demo.Basics.DataTypes;
 
-public class TypeCasting
+public static class TypeCasting
 {
+    public static string getResult(int first, string second)
+    {
+        return first + second;
+    }
 
+    public static int getResult(int first, int second)
+    {
+        return first + second;
+    }
+
+    public static void ExplicitCasting()
+    {
+        decimal myDecimal = 3.14m;
+        Console.WriteLine($"decimal: {myDecimal}");
+
+        /// The term widening conversion means that you're attempting to convert a value from a data type that could hold less information to a data type that can hold more information. In this case, a value stored in a variable of type int converted to a variable of type decimal, doesn't lose information. When you know you're performing a widening conversion, you can rely on implicit conversion. The compiler handles implicit conversions.
+        /// 
+
+        int myInt = (int)myDecimal;
+        Console.WriteLine($"int: {myInt}");
+
+        // -----------------------------------------------------------------------------------------------
+
+        decimal myDecimal1 = 3.14m;
+        Console.WriteLine($"decimal: {myDecimal1}");
+
+        /// explicit conversion => Loss of Data. () is a Cast-Operator.
+        int myInt1 = (int)myDecimal;
+        Console.WriteLine($"int: {myInt1}");
+    }
+
+    public static void useToStringMethod()
+    {
+        int first = 5;
+        int second = 7;
+        string message = first.ToString() + second.ToString();
+        Console.WriteLine(message);
+    }
+
+    public static void useParseMethod()
+    {
+        string first = "5";
+        string second = "7";
+        int sum = int.Parse(first) + int.Parse(second);
+        Console.WriteLine(sum);
+    }
 }
--- C#_Coding_Demo/Program.cs
+++ C#_Coding_Demo/Program.cs
@@ -1,2 +1,39 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using C__Coding_Demo.Basics.DataTypes;
+using System;
+
+Console.WriteLine("C# Program Demo's");
+
+/// Basics => DataTypes => TypeCasting => getResult(int, string/int) ///
+///
+/*  -------------------------------------------------------------
+int first = 10;
+string second = "20";
+// int second = 20;
+
+try
+{
+    string Result = TypeCasting.getResult(first, second);
+    // int Result = TypeCasting.getResult(first, second);
+    Console.WriteLine($"Result is: {Result}");
+}
+
+catch (Exception ex)
+{
+    System.Console.WriteLine($"Error Occured: {ex.Message}");
+}
+
+*/
+
+/// Basics => DataTypes => TypeCasting => ExplicitCasting() ///
+
+TypeCasting.ExplicitCasting();
+
+
+/// Basics => DataTypes => TypeCasting => useToStringMethod() ///
+
+TypeCasting.useToStringMethod();
+
+/// Basics => DataTypes => TypeCasting => useParseMethod() ///
+
+TypeCasting.useParseMethod();
