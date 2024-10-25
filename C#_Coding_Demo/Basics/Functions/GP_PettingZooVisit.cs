3bca772cc469f9900637459534c7cd3f2c54bf6b
ganeshnaykawadi <ganeshnaykawadi@gmail.com>
2024-10-25 19:25:47 +0530

    PettingZoo Project advancement

---
+++ C#_Coding_Demo/Basics/Functions/GP_PettingZooVisit.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace C__Coding_Demo.Basics.Functions;
+
+public class GP_PettingZooVisit
+{
+    string[] pettingZoo =
+    {
+        "alpacas", "capybaras", "chickens", "ducks", "emus", "geese",
+        "goats", "iguanas", "kangaroos", "lemurs", "llamas", "macaws",
+        "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
+    };
+
+    // RandomizeAnimals();
+
+    // AssignGroup();
+
+    // string[,] group = AssignGroup();
+
+    // Console.WriteLine("School A");
+
+    // PrintGroup(group);
+
+}
