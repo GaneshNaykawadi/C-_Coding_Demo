13b79714042c12f301bf60f8b286cee8bf617ef1
ganeshnaykawadi <ganeshnaykawadi@gmail.com>
2024-11-26 20:29:16 +0530

    Faulty syntax corrected.

--- C#_Coding_Demo/obj/Debug/net8.0/C#_Coding_Demo.AssemblyInfo.cs
+++ C#_Coding_Demo/obj/Debug/net8.0/C#_Coding_Demo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#_Coding_Demo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd47d9e95bd45abc802dc60575fa3af03aa59c3f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+470485f113ff1dd891353aeabfa68aa0848973ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#_Coding_Demo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#_Coding_Demo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
