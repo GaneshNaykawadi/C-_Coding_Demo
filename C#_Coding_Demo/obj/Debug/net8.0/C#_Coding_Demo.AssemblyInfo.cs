00baae0c5db2ba3907310ef09db729b346437339
ganeshnaykawadi <ganeshnaykawadi@gmail.com>
2025-01-21 10:34:05 +0530

    commited on 21 Jan, 2025.

--- C#_Coding_Demo/obj/Debug/net8.0/C#_Coding_Demo.AssemblyInfo.cs
+++ C#_Coding_Demo/obj/Debug/net8.0/C#_Coding_Demo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#_Coding_Demo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6e2223c172f5914addc45d3a7731ea2cd550828")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33c9b43ab898b6e17db33dd8abeabe63b0091529")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#_Coding_Demo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#_Coding_Demo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
